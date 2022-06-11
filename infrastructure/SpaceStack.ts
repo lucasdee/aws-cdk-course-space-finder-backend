4dba44d28d35fc92e712872231fd42f918077360
lucasdee <lucasdee@gmx.com>
2022-06-11 16:29:38 +0200

    22. AWS DynamoDB with CDK

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -4,22 +4,25 @@ import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambd
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 
 import { join } from 'path';
+import { GenericTable } from './GenericTable';
 
 export class SpaceStack extends Stack {
-  private api = new RestApi(this, 'SpaceApi')
+  private api = new RestApi(this, 'SpaceApi');
+
+  private spacesTable = new GenericTable('SpacesTable', 'spaceId', this);
 
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    const helloLambda = new LambdaFunction(this, "helloLambda", {
+    const helloLambda = new LambdaFunction(this, 'helloLambda', {
       runtime: Runtime.NODEJS_16_X,
       code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
-      handler: 'hello.main'
-    })
+      handler: 'hello.main',
+    });
 
     // Hello Api lambda integration:
     const helloLambdaIntegration = new LambdaIntegration(helloLambda);
     const helloLambdaResource = this.api.root.addResource('hello');
-    helloLambdaResource.addMethod('GET', helloLambdaIntegration)
+    helloLambdaResource.addMethod('GET', helloLambdaIntegration);
   }
 }
