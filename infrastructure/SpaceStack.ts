a2a12e93ea67359c07b5dd104c6b15ec5a37e47e
lucasdee <lucasdee@gmx.com>
2022-06-11 15:27:35 +0200

    21. AWS API Gateway and Lambda

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,17 +1,25 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 
 import { join } from 'path';
 
 export class SpaceStack extends Stack {
+  private api = new RestApi(this, 'SpaceApi')
+
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    new LambdaFunction(this, "helloLambda", {
+    const helloLambda = new LambdaFunction(this, "helloLambda", {
       runtime: Runtime.NODEJS_16_X,
       code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
       handler: 'hello.main'
     })
+
+    // Hello Api lambda integration:
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource('hello');
+    helloLambdaResource.addMethod('GET', helloLambdaIntegration)
   }
 }
