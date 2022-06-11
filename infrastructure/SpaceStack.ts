5912076c1ef6c0f0fc552119371c79ff4a92118e
lucasdee <lucasdee@gmx.com>
2022-06-11 19:41:50 +0200

    25. CDK Node Lambda

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 import { join } from 'path';
 import { GenericTable } from './GenericTable';
@@ -18,6 +19,11 @@ export class SpaceStack extends Stack {
       runtime: Runtime.NODEJS_16_X,
       code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
       handler: 'hello.main',
+    });
+
+    const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
+      entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
+      handler: 'handler'
     });
 
     // Hello Api lambda integration:
