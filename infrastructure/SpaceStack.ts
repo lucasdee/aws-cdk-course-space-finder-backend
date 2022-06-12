355b03fb0f7624df8f451d3dd2760b562cd565f6
lucasdee <lucasdee@gmx.com>
2022-06-12 17:37:18 +0200

    30. Using the AWS sdk

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,8 +1,13 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+} from 'aws-cdk-lib/aws-lambda';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 import { join } from 'path';
 import { GenericTable } from './GenericTable';
@@ -23,8 +28,14 @@ export class SpaceStack extends Stack {
 
     const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
       entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
-      handler: 'handler'
+      handler: 'handler',
     });
+
+    const s3ListPolicy = new PolicyStatement();
+    s3ListPolicy.addActions('s3:ListAllMyBuckets');
+    s3ListPolicy.addResources('*');
+
+    helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
 
     // Hello Api lambda integration:
     const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs);
