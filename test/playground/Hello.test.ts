7e31d406d5be9288c9fe0bb9f7343caa6448446d
lucasdee <lucasdee@gmx.com>
2022-06-13 20:27:29 +0200

    37. Scan operation

--- test/playground/Hello.test.ts
+++ test/playground/Hello.test.ts
@@ -1,9 +1,6 @@
-import { handler } from '../../services/SpacesTable/Create';
+import { handler } from '../../services/SpacesTable/Read';
 
-const event = {
-  body: {
-    location: 'Paris',
-  },
-};
-
-handler(event as any, {} as any);
+const result = handler({} as any, {} as any).then((apiRes) => {
+  const items = JSON.parse(apiRes.body);
+  debugger;
+});
