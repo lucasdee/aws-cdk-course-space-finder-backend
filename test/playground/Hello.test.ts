5a37a535092722cee32cd54299a58d7a11f3e49d
lucasdee <lucasdee@gmx.com>
2022-06-13 21:40:15 +0200

    40. Update operation

--- test/playground/Hello.test.ts
+++ test/playground/Hello.test.ts
@@ -1,10 +1,13 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
-import { handler } from '../../services/SpacesTable/Read';
+import { handler } from '../../services/SpacesTable/Update';
 
 const event: APIGatewayProxyEvent = {
   queryStringParameters: {
-    location: 'London',
+    spaceId: 'be44c721-61c4-44ca-afab-3c363db43740',
   },
+  body: {
+    location: 'new location'
+  }
 } as any;
 
 const result = handler(event as any, {} as any).then(apiRes => {
