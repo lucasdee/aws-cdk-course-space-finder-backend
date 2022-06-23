a0fe7c798d60430fdb2b66669e8b08a248d716ed
lucasdee <lukasz.dolan@gmail.com>
2022-06-23 14:14:24 +0200

    52. Testing CDK with Cognito

---
+++ test/Auth/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  REGION: 'eu-east-1',
+  USER_POOL_ID: 'us-east-1_f6vPd9oLb',
+  APP_CLIENT_ID: '3pd3bkdeh4g3dt6nbcom9gm1e9',
+  TEST_USER_NAME: 'lucasdee2',
+  TEST_USER_PASSWORD: 'RUWHr85aUvCmLiQm#',
+};
