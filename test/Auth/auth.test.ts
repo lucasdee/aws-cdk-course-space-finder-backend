5c42313fc484b7df0019385627331b578849e48e
lucasdee <lukasz.dolan@gmail.com>
2022-06-23 16:19:16 +0200

    56. Getting AWS temporary credentials

--- test/Auth/auth.test.ts
+++ test/Auth/auth.test.ts
@@ -1,6 +1,14 @@
 import { AuthService } from './AuthService';
 import { config } from './config';
+import * as AWS from 'aws-sdk';
 
-const authService = new AuthService();
+async function callStuff() {
+  const authService = new AuthService();
 
-const user = authService.login(config.TEST_USER_NAME, config.TEST_USER_PASSWORD);
+  const user = await authService.login(config.TEST_USER_NAME, config.TEST_USER_PASSWORD);
+  await authService.getAWSTemporaryCreds(user);
+  const someCreds = AWS.config.credentials;
+  console.log('1');
+}
+
+callStuff();
--- test/Auth/config.ts
+++ test/Auth/config.ts
@@ -1,7 +1,8 @@
 export const config = {
-  REGION: 'eu-east-1',
+  REGION: 'us-east-1',
   USER_POOL_ID: 'us-east-1_f6vPd9oLb',
   APP_CLIENT_ID: '3pd3bkdeh4g3dt6nbcom9gm1e9',
+  IDENTITY_POOL_ID: 'us-east-1:f78a6ffe-7677-4e9a-8777-4993b77870ab',
   TEST_USER_NAME: 'lucasdee2',
   TEST_USER_PASSWORD: 'RUWHr85aUvCmLiQm#',
 };
